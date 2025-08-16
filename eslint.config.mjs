bd77e35ef53d840cc4fd2aa0a2c218b16a8eda39
Recuperiamo <r3cuperiamo@gmail.com>
2025-08-16 22:35:19 +0200

    chore: aggiunto @next/eslint-plugin-next per supporto lint Next.js in flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginNext from "@next/eslint-plugin-next";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -21,11 +22,28 @@ export default defineConfig([
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    plugins: {
+      js,
+      react: pluginReact,
+      "@next/next": pluginNext
+    },
+    extends: [
+      "js/recommended",
+      "plugin:react/recommended",
+      "plugin:@next/next/recommended"
+    ],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
     },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off" // Disattiva se usi TS o non usi prop-types
+    }
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
