8a688b8dcfcb58a1e817083c9015303d42c8ab54
Recuperiamo <r3cuperiamo@gmail.com>
2025-10-07 00:51:49 +0200

    Fix: aggiorna import authOptions e risolve errori build

--- app/api/lavagna/realtime/route.js
+++ app/api/lavagna/realtime/route.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "../../auth/[...nextauth]/authOptions";
 import { prisma } from "../../../lib/prisma";
 import { addClient, removeClient, broadcast } from "../../../lib/realtime/lavagnaHub";
 
