9ae60ed5f8ebcdbae53cbebddbce58863b7f6023
Recuperiamo <r3cuperiamo@gmail.com>
2025-10-29 16:57:12 +0100

    Force rebuild: add comment to break Vercel cache for lavagna API

--- app/api/lavagna/route.js
+++ app/api/lavagna/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { prisma } from "../../../lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/authOptions";
+
+// Force rebuild - prisma import fix (Oct 29, 2025)
 /**
  * GET /api/lavagna?attivitaId=123
  * Titolo persistito: "dd/MM/yyyy HH:mm"
