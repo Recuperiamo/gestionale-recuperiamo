4a21c6dc85fa6895f99277eb312ddd892996d6e4
Recuperiamo <r3cuperiamo@gmail.com>
2025-10-29 13:37:59 +0100

    Fix: import paths in richieste-lavagna API

--- app/api/richieste-lavagna/route.js
+++ app/api/richieste-lavagna/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../auth/[...nextauth]/route";
-import prisma from "../../../../lib/prisma";
+import { authOptions } from "../auth/[...nextauth]/authOptions";
+import { prisma } from "../../../lib/prisma";
 
 export async function GET(req) {
   try {
