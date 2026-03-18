8fa36360acce996ee23d5139ff3e0dcdaa9a38ee
Recuperiamo <r3cuperiamo@gmail.com>
2026-03-18 23:11:35 +0100

    fix: rimuovi LavagnaCanvas.tsx spazzatura dalla root

--- LavagnaCanvas.tsx
+++
@@ -1,6 +0,0 @@
-// Commented out to disable automatic spectator mode activation
-// setSpectatorMode(online)
-
-if (!spectatorModeRef.current) setSpectatorMode(true) // Commented out to disable automatic spectator mode activation
-
-// (rest of the code unchanged)
