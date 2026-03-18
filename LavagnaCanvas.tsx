338496df5aa583f2bcdb2f3735032443cb7207ed
Recuperiamo <r3cuperiamo@gmail.com>
2026-03-18 14:02:47 +0100

    Comment out automatic spectator mode activation lines in LavagnaCanvas.tsx

---
+++ LavagnaCanvas.tsx
@@ -0,0 +1,6 @@
+// Commented out to disable automatic spectator mode activation
+// setSpectatorMode(online)
+
+if (!spectatorModeRef.current) setSpectatorMode(true) // Commented out to disable automatic spectator mode activation
+
+// (rest of the code unchanged)
